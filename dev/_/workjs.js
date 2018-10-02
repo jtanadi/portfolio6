d4497135c827f1fd9cbce704ee4145558a1608f8
Jesen Tanadi <mail@jesentanadi.com>
2018-10-02 08:29:40 -0400

    clean up

--- dev/_/workjs.js
+++ dev/_/workjs.js
@@ -7,7 +7,6 @@ let menuStatus = false;
 menuBar.addEventListener("mouseup", () => {
   if(!menuStatus) {
     menuStatus = true;
-    // menuBar.style.height = "100vh";
     // menuText.style.display = "none";
     menuNav.style.height = "100vh";
   } else {
